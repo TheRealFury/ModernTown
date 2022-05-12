90c6f8d5bb3a33456804441dcbb511edb22125a0
TheRealFury <76801758+TheRealFury@users.noreply.github.com>
2022-05-12 16:42:24 +0530

    Added Base files

---
+++ JS/sketch.js
@@ -0,0 +1,15 @@
+
+
+function setup(){
+  
+  // Set the stage - the canvas.
+  createCanvas(1350,650);
+}
+
+function draw(){
+
+  // The background colour.
+  background(0);
+
+  drawSprites();
+}
